f453fbadbde825a8c48c7929ec2973c60e7e7ff0
Lukasz <skotniczny@outlook.com>
2016-05-24 17:06:17 +0200

    code refactoring

--- js/main.js
+++ js/main.js
@@ -1,73 +1,74 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+function $qs(selector) {
+  return document.querySelector(selector);
+}
 
-  // hamburger menu
+function $qsa(selector, scope) {
+  return (scope || document).querySelectorAll(selector);
+}
 
-  var menu = document.querySelector(".main-menu");
-  menu.classList.add('hidden');
-
-  var hamburgerBtn = document.querySelector("#hamburger");
-  hamburgerBtn.addEventListener("click", function(event) {
-    this.nextElementSibling.classList.toggle("hidden");
-  });
+function typingAnimation(node, options) {
+  options = options || {};
+  var delay = options.delay || 1000;
+  var duration = options.duration || 2000;
+  var typingSpeed = options.typingSpeed || 200;
+  var deletingSpeed = options.deletingSpeed || 100;
+  var loop = options.loop;
   
-  // typing animation
-
-  var heading = document.querySelector("#we-love");
-  var arr = ["to write code", "web design", "social marketing"];
+  var arr = node.getAttribute("data-phrase").split(" | ");
   var arrIdx = 0;
-  var counter = 0;
-  var text = arr[arrIdx];
-  var end = text.length;
-  var out = "";
+  var textToDelete = arr[arrIdx];
+  var textToType = arr[arrIdx + 1] || arr[0];
+  var typingCounter = 0;
+  var deletingCounter = textToDelete.length;
   
-  function typing () {
-    console.log(counter, end);
-    if (end > 0) {
-      end -= 1;
-      heading.textContent = text.slice(0, end);
-      setTimeout(typing, 100);
-    } else if (counter < text.length) {
-      text = arr[arrIdx + 1] || arr[0]; 
-      counter += 1;
-      heading.textContent = text.slice(0,counter);
-      setTimeout(typing, 200);
+  function typing() {
+    if (deletingCounter > 0) {
+      deletingCounter -= 1;
+      node.textContent = textToDelete.slice(0, deletingCounter);
+      setTimeout(typing, deletingSpeed);
+    } else if (typingCounter < textToType.length) {
+      typingCounter += 1;
+      node.textContent = textToType.slice(0, typingCounter);
+      setTimeout(typing, typingSpeed);
     } else if (arrIdx < arr.length - 1) {
       arrIdx += 1;
-      counter = 0;
-      text = arr[arrIdx];
-      end = text.length;
-      setTimeout(typing, 200);
+      textToDelete = arr[arrIdx];
+      textToType = arr[arrIdx + 1] || arr[0];
+      typingCounter = 0;
+      deletingCounter = textToDelete.length;
+      setTimeout(typing, duration);
     } else {
-      heading.classList.add("disabled");
+      if (loop) {
+        arrIdx = -1;
+        typing();
+      } else {
+        node.classList.add("disabled");
+      }
     }
   }
+  
+  setTimeout(typing, delay);
+}
 
-  setTimeout(typing, 3000);
+function makeAccordion(node) {
 
-  // hide faq answers
-
-  var faqAnswers = document.querySelectorAll(".faq dd");
-
+  var faqQuestions = $qsa("dt a", node);
+  var faqAnswers = $qsa("dd", node);
+  
   function hideAnswers() {
     for (var i = 0; i < faqAnswers.length; i++) {
       faqAnswers[i].classList.add("hidden");
     }
   }
-
   hideAnswers();
-
+  
   function removeActive() {
-    var faqAnchors = document.querySelectorAll(".faq dt a");
-    for (var i = 0; i < faqAnchors.length; i++) {
-      faqAnchors[i].classList.remove("active", "icon-cancel-circled");
-      faqAnchors[i].classList.add("icon-plus-circled");
+    for (var i = 0; i < faqQuestions.length; i++) {
+      faqQuestions[i].classList.remove("active", "icon-cancel-circled");
+      faqQuestions[i].classList.add("icon-plus-circled");
     }
-  }
-
-  // faq accordion
-
-  var faqQuestions = document.querySelectorAll(".faq dt a");
-
+  }    
+  
   function accordion(event) {
     hideAnswers();
     removeActive();
@@ -76,46 +77,70 @@ document.addEventListener("DOMContentLoaded", function(event) {
     this.classList.add("active");
     this.classList.add("icon-cancel-circled");
     this.parentNode.nextElementSibling.classList.remove("hidden");
-  }
+  }    
   
   for (var i = 0; i < faqQuestions.length; i++) {
       faqQuestions[i].addEventListener("click", accordion, false);
-  }
+  }    
+}
 
-  // filter buttons
+function filterElements(filterBtnsContainer, elmsToFilterContainer) {
 
-  var filterBtns = document.querySelectorAll(".filter-buttons");
+  var elmsToFilter = $qsa("li", elmsToFilterContainer);
 
-  function filterWorks(event) {
-    var portfolioElms = document.querySelectorAll(".works-list li");
+  function filter(event) {
+    
     var target = event.target;
-    if (target.tagName == "BUTTON") {
-      if(document.querySelectorAll("button.active")[0]) {
-        document.querySelectorAll("button.active")[0].classList.remove("active");
-      }
+
+    if (target.tagName.toLowerCase() === "button") {
+      $qsa(".active", filterBtnsContainer)[0].classList.remove("active");
       target.classList.add("active");
-      var filter = target.id;
-      for (var i = 0; i < portfolioElms.length; i++) {
-        portfolioElms[i].classList.remove("hidden");
-        var elmData = portfolioElms[i].getAttribute("data-cat");
-        if (filter !== elmData && filter !== "all") {
-          portfolioElms[i].classList.add("hidden");
+      
+      var filterBtn = target.id;
+      
+      for (var i = 0; i < elmsToFilter.length; i++) {
+        elmsToFilter[i].classList.remove("hidden");
+      
+        var elmData = elmsToFilter[i].getAttribute("data-cat");
+        
+        if (filterBtn !== elmData && filterBtn !== "all") {
+          elmsToFilter[i].classList.add("hidden");
         }
       }
     }
   }
 
-  for (i = 0; i < filterBtns.length; i++) {
-    filterBtns[i].addEventListener("click", filterWorks, false);
-  }
+  filterBtnsContainer.addEventListener("click", filter, false);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // hamburger menu
+
+  $qs(".main-menu").classList.add('hidden');
+
+  $qs("#hamburger").addEventListener("click", function(event) {
+    this.nextElementSibling.classList.toggle("hidden");
+  });
+  
+  
+  typingAnimation($qs("#we-love"), {
+    duration: 3000
+  });
+
+
+  makeAccordion($qs(".faq"));
+
+
+  filterElements($qs(".filter-buttons"), $qs(".works-list"));
 
   // plan switcher
 
-  document.querySelector("#monthly").checked = true;
-  var selectPlan = document.querySelectorAll("input[name=plan");
+  $qs("#monthly").checked = true;
+  var selectPlan = $qsa("input[name=plan]");
   
   function planSwitcher () {
-    var elems = document.querySelectorAll(".plan-container");
+    var elems = $qsa(".plan-container");
     for (var i = 0; i < elems.length; i++) {
       if (this.value == "yearly") {
         elems[i].classList.add("rotate");
